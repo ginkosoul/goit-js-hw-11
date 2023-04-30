caca7ac8348ff942dd5aadf6b5e9796b538cfba3
ginkosoul <ginkosoul@gmail.com>
2023-04-30 22:47:52 +0300

    bug in galleryAPI 3

--- src/js/galleryAPI.js
+++ src/js/galleryAPI.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 export default class GalleryAPI{
     static #API_KEY = '35837403-e3eb495b2214d16b5d801685b'
     static #BASE_URL = 'https://pixabay.com/api/'
-    #params = {}
+    params = {}
 
     constructor(){
-        this.#params.key = GalleryAPI.#API_KEY
-        this.#params.page = 1
-        this.#params.per_page = 18
-        this.#params.q = ''
-        this.#params.image_type = 'photo'
-        this.#params.orientation = 'horizontal'
-        this.#params.safesearch = true
+        this.params.key = GalleryAPI.#API_KEY
+        this.params.page = 1
+        this.params.per_page = 18
+        this.params.q = ''
+        this.params.image_type = 'photo'
+        this.params.orientation = 'horizontal'
+        this.params.safesearch = true
         this.isListEnd = false
         this.totalHits = 0
         this.count = 0
     }
 
     fetchImages() {
-        const params = this.#params
+        const params = this.params
     
         return axios.get(GalleryAPI.#BASE_URL,{params})
         .then(r => r.data)
         .then(r => {
             this.totalHits = r.totalHits
-            if (r.totalHits > (this.#params.per_page * this.#params.page)) {
+            if (r.totalHits > (this.params.per_page * this.params.page)) {
                 this.isListEnd = false;
                 this.pageInc()
             } else {
@@ -41,22 +41,22 @@ export default class GalleryAPI{
     }
     
     pageInc(){
-        this.#params.page++
-        this.count += this.#params.per_page
+        this.params.page++
+        this.count += this.params.per_page
     }
 
     get q(){
-        return this.#params.q
+        return this.params.q
     }
     set q(text){
-        this.#params.q = text.trim()
-        this.#params.page = 1
+        this.params.q = text.trim()
+        this.params.page = 1
         this.count = 0;
     }
     get perPage(){
-        return this.#params.per_page
+        return this.params.per_page
     }
     set perPage(number){
-        this.#params.per_page = number
+        this.params.per_page = number
     }
 }
